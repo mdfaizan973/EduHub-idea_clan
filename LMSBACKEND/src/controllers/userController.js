e6d6fe430e7c34b4bd4c8e0b34a9acc0ba3d8a63
mdfaizan973 <faizan.md9735@gmail.com>
2024-03-10 14:17:17 +0530

    able to make put request in profile

--- LMSBACKEND/src/controllers/userController.js
+++ LMSBACKEND/src/controllers/userController.js
@@ -35,4 +35,30 @@ const deleteUserById = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, getAllUsers, deleteUserById };
+const updateUserProfile = async (req, res) => {
+  const { name, email, password } = req.body;
+  const userId = req.params.id;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { name, email, password },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+module.exports = {
+  registerUser,
+  getAllUsers,
+  deleteUserById,
+  updateUserProfile,
+};
--- LMSBACKEND/src/routes/userRoutes.js
+++ LMSBACKEND/src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const {
   registerUser,
   getAllUsers,
   deleteUserById,
+  updateUserProfile,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -11,4 +12,5 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.get("/users", getAllUsers);
 router.delete("/:id", deleteUserById);
+router.put("/:id", updateUserProfile);
 module.exports = router;
