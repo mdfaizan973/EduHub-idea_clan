ebd22899e9c80b512d98ca7ad62923195d3dc33f
mdfaizan973 <faizan.md9735@gmail.com>
2024-03-10 19:27:25 +0530

    added profile

--- lms/src/Pages/Admin/NavbarHead.jsx
+++ lms/src/Pages/Admin/NavbarHead.jsx
@@ -1,4 +1,4 @@
-import { Button, Layout } from "antd";
+import { Button, Layout, Modal, Space } from "antd";
 const { Header } = Layout;
 import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
 // import { Link as RouterLink } from "react-router-dom";
@@ -9,6 +9,39 @@ export default function NavbarHead() {
     setTimeout(() => {
       window.location.href = "/";
     }, 800);
+  };
+  const admin = {
+    name: "Admin123",
+    email: "admin123@admin.com",
+    password: "1234pass",
+    imageUrl:
+      "https://i.pinimg.com/564x/0c/ce/24/0cce244c8456e9632233b1921450f5af.jpg", // URL to admin's image
+  };
+  const showModal = () => {
+    Modal.info({
+      title: "Admin Information",
+      content: (
+        <div>
+          <img
+            src={admin.imageUrl}
+            alt="Admin"
+            style={{ width: 100, borderRadius: "50%", marginRight: 20 }}
+          />
+          <Space direction="vertical">
+            <div>
+              <strong>Name:</strong> {admin.name}
+            </div>
+            <div>
+              <strong>Email:</strong> {admin.email}
+            </div>
+            <div>
+              <strong>Password:</strong> {admin.password}
+            </div>
+          </Space>
+        </div>
+      ),
+      onOk() {},
+    });
   };
   return (
     <div>
@@ -46,6 +79,7 @@ export default function NavbarHead() {
             type="text"
             icon={<UserOutlined />}
             style={{ marginRight: 20 }}
+            onClick={showModal}
           />
           {/* </RouterLink> */}
           <Button
