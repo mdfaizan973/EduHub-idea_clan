1dfa6faba6a89b2cbfe069e4e0235a5455cdbef3
mdfaizan973 <faizan.md9735@gmail.com>
2024-12-08 17:01:14 +0530

    new chnages

--- lms/src/Pages/Admin/CourseCreation.jsx
+++ lms/src/Pages/Admin/CourseCreation.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import NavbarHead from "./NavbarHead";
 import CourseManage from "./CourseManage";
 import axios from "axios";
+
 export default function CourseCreation() {
   return (
     <div>
